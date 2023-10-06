5505c9c69b100df01ecfd39a6a2333f89533ce1c
SurajMaurya <surajm315760@gmail.com>
2023-10-06 16:57:01 +0530

    ok commit

--- app/src/main/java/com/example/newtestapplication/Roomdatabase/ContactDatabase.kt
+++ app/src/main/java/com/example/newtestapplication/Roomdatabase/ContactDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import com.example.newtestapplication.Modal.Meal
 
 
-//@Database(entities = [Meal::class], version = 1, exportSchema = false)
-//abstract class ContactDatabase : RoomDatabase() {
-//    abstract fun contactDao(): ContactDAO
-//}
+@Database(entities = [Meal::class], version = 1, exportSchema = false)
+abstract class ContactDatabase : RoomDatabase() {
+    abstract fun contactDao(): ContactDAO
+}
