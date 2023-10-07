711462f421e60405a81eb16d0087b30ea1629eb6
SurajMaurya <surajm315760@gmail.com>
2023-10-07 11:41:47 +0530

    ok complate;

---
+++ app/src/main/java/com/example/newtestapplication/MenuLisTActivity.kt
@@ -0,0 +1,12 @@
+package com.example.newtestapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuLisTActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_lis_tactivity)
+    }
+}
