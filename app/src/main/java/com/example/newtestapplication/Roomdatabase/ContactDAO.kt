770dc7493cba91dc11561d8547f3cf5f1fac799e
SurajMaurya <surajm315760@gmail.com>
2023-10-07 11:02:18 +0530

    ok complate

--- app/src/main/java/com/example/newtestapplication/Roomdatabase/ContactDAO.kt
+++ app/src/main/java/com/example/newtestapplication/Roomdatabase/ContactDAO.kt
@@ -2,17 +2,22 @@ package com.example.newtestapplication.Roomdatabase
 
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import com.example.newtestapplication.Modal.Meal
 
 @Dao
 interface ContactDAO {
     @Query("SELECT * FROM contact")
     fun getContact(): LiveData<List<Meal>>
-
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertContact(meal: Meal)
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    fun updateContact(meal: Meal)
+    @Delete
+    fun deleteContact(meal: Meal)
 
 }
